da52cb66767db3eb31c2285c87467c3c23f594bf
Audrey Wang <audreywang2006@gmail.com>
2022-03-19 23:45:25 -0700

    add severity madlib

--- web/script.js
+++ web/script.js
@@ -39,3 +39,34 @@ function draw() {
 //   erase();
 //   background(100);
 //   }
+
+var symp = document.getElementById("symptoms");
+var symptoms = parseInt(symp.value);
+
+var st = document.getElementById("stand");
+var stand = parseInt(st.value);
+
+var wh = document.getElementById("wheelchair");
+var wheelchair = parseInt(wh.value);
+
+var total = symptoms + stand + wheelchair;
+var show;
+var desc;
+if (total >= 6) {
+  show = "SEVERE";
+  desc = ":0";
+} else if (total < 6 && total > 4) {
+  show = "MODERATE";
+  desc =
+    "You've been down the road but haven't rolled yet so continue to practice the resources attached below.";
+} else {
+  show = "MILD";
+  desc =
+    "Say it with me! Yaayyy! I'm all pumped up. That's pretty good, and I think I have energy for at least 4 more hills. I'm going to run and run and run! I can do this. I'm too youthful!!! I will keep up with my healthy mindset and happy soul. It's so nice to see the scale moving in the right direction. I am stable in all ways! LOOK! I can stand and walk gracefully and intently! I am able to control my momentum! To continue to prevent the futhering of Parkinson's, see the resources attached below.";
+}
+function onSubmit() {
+  // p.innerHTML += "We have detected that you have mild symtoms."
+  // document.getElementById(“result”).innerText +=  “data”;
+
+  alert(`${total} We have detected that you have ${show} symtoms. ${desc}`);
+}
